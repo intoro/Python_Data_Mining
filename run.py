57b437046d4d834f38e6ec59ee73275ce89585ea
Brian <mayrose.dg@gmail.com>
2017-10-17 12:20:05 -0700

    added

--- run.py
+++ run.py
@@ -59,10 +59,39 @@ print("As a percentage, that is {0:.1f}%.".format(100 * confidence))
 
 
 
+from collections import defaultdict
+# Now compute for all possible rules
+valid_rules = defaultdict(int)
+invalid_rules = defaultdict(int)
+num_occurences = defaultdict(int)
+
+for sample in X:
+    for premise in range(n_features):
+        if sample[premise] == 0: continue
+        # Record that the premise was bought in another transaction
+        num_occurences[premise] += 1
+        for conclusion in range(n_features):
+            if premise == conclusion:  # It makes little sense to measure if X -> X.
+                continue
+            if sample[conclusion] == 1:
+                # This person also bought the conclusion item
+                valid_rules[(premise, conclusion)] += 1
+            else:
+                # This person bought the premise, but not the conclusion
+                invalid_rules[(premise, conclusion)] += 1
+support = valid_rules
+confidence = defaultdict(float)
+for premise, conclusion in valid_rules.keys():
+    confidence[(premise, conclusion)] = valid_rules[(premise, conclusion)] / num_occurences[premise]
 
 
-
-
+for premise, conclusion in confidence:
+    premise_name = features[premise]
+    conclusion_name = features[conclusion]
+    print("Rule: If a person chooses {0} they will also choose {1}".format(premise_name, conclusion_name))
+    print(" - Confidence: {0:.3f}".format(confidence[(premise, conclusion)]))
+    print(" - Support: {0}".format(support[(premise, conclusion)]))
+    print("")
 
 
 
